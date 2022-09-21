80e61cd165ca8970c50af04189675c2f38fb05cb
GoblinsWang <806586736@qq.com>
2022-09-21 20:55:02 +0800

    mod

--- car_attr_recognition/configs/resnet50_finetune.py
+++ car_attr_recognition/configs/resnet50_finetune.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append("../../configs/")
 _base_ = [
-    './_base_/models/resnet50.py',
-    './_base_/datasets/cifar10_bs16.py', './_base_/default_runtime.py'
+    '../../configs/_base_/models/resnet50.py',
+    '../../configs/_base_/datasets/cifar10_bs16.py', '../../configs/_base_/default_runtime.py'
 ]
 
 # 模型设置
